a187d077c64001978955e4b613a5473c548998fb
ijcool999 <ishanjain2174@gmail.com>
2025-03-24 03:43:58 +0530

    SChools bulk  upload

--- src/models/schoolModel.js
+++ src/models/schoolModel.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 
 const schoolSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  location: {
+  
     city: { type: String, required: true },
-    address: { type: String, required: true }
-  },
+    address: { type: String, required: true },
+  
   image: { type: String }, // URL or path to the uploaded image
   createdAt: { type: Date, default: Date.now }
 });
