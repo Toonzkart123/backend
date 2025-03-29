e2258be3c0890fb98d2a26f55b265960ed8d8d23
ijcool999 <ishanjain2174@gmail.com>
2025-03-29 19:51:16 +0530

    Middlware removed in shopbydemand - UPDATED

--- src/models/bookRequestModel.js
+++ src/models/bookRequestModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const bookRequestSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User"},
   books: [
     {
       title: { type: String, required: true },
